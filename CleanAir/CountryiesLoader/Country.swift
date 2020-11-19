cde4ebe2c7c8319d2b75524bef1dfbd31b5141f4
Marko Engelman <engleman.marko@gmail.com>
2020-11-20 00:42:13 +0100

    Add 'Country'.

---
+++ CleanAir/CountryiesLoader/Country.swift
@@ -0,0 +1,15 @@
+//
+//  Country.swift
+//  CleanAir
+//
+//  Created by Marko Engelman on 20/11/2020.
+//
+
+import Foundation
+
+struct Country {
+  let name: String
+  let totalNumberOfMeasurements: Int
+  let numberOfMeasuredCities: Int
+  let numberOfMeasuringLocations: Int
+}
