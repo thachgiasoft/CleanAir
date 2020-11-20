0ee38fdc3e118e9d2a5f6bb0f5998752476be36b
Marko Engelman <engleman.marko@gmail.com>
2020-11-20 12:53:00 +0100

    Add 'ResourceListSwiftUIViewAdapter'.

---
+++ CleanAir/UI/Shared/Views/ResourceListSwiftUIViewAdapter.swift
@@ -0,0 +1,42 @@
+//
+//  ResourceListSwiftUIViewAdapter.swift
+//  CleanAir
+//
+//  Created by Marko Engelman on 20/11/2020.
+//
+
+import Foundation
+
+typealias ResourceListViewViewModel = ResourceListSwiftUIViewAdapter
+class ResourceListSwiftUIViewAdapter<ResourceViewModel>: ObservableObject {
+  let onAppear: () -> Void
+  @Published var resource: ResourceViewModel
+  @Published var isLoading: Bool = false
+  @Published var error: String?
+  
+  init(onAppear: @escaping () -> Void, resource: ResourceViewModel) {
+    self.onAppear = onAppear
+    self.resource = resource
+  }
+}
+
+// MARK: - ResourceView
+extension ResourceListSwiftUIViewAdapter: ResourceView {
+  func show(resourceViewModel: ResourceViewModel) {
+    self.resource = resourceViewModel
+  }
+}
+
+// MARK: - ResourceLoadingView
+extension ResourceListSwiftUIViewAdapter: ResourceLoadingView {
+  func show(loadingViewModel: ResourceLoadingViewModel) {
+    self.isLoading = loadingViewModel.isLoading
+  }
+}
+
+// MARK: - ResourceErrorView
+extension ResourceListSwiftUIViewAdapter: ResourceErrorView {
+  func show(errorViewModel: ResourceErrorViewModel) {
+    error = errorViewModel.error
+  }
+}
