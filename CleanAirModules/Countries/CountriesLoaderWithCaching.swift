6256dcbeb9f764ae210252a4d14dbe6bf14eb20e
Marko Engelman <engleman.marko@gmail.com>
2020-11-20 17:12:01 +0100

    Compose 'CountriesLoaderWithCaching' with caching.

--- CleanAirModules/Countries/CountriesLoaderWithCaching.swift
+++ CleanAirModules/Countries/CountriesLoaderWithCaching.swift
@@ -8,5 +8,23 @@
 import Foundation
 
 public class CountriesLoaderWithCaching {
+  let loader: CountriesLoader
+  let cache: ResourceCache<[Country], CountriesStorage>
   
+  public init(loader: CountriesLoader, cache: ResourceCache<[Country], CountriesStorage>) {
+    self.loader = loader
+    self.cache = cache
+  }
+}
+
+// MARK: - CountriesLoader
+extension CountriesLoaderWithCaching: CountriesLoader {
+  public func load(completion: @escaping (Result<[Country], Error>) -> Void) {
+    loader.load { [weak self] result in
+      if case let .success(countries) = result {
+        self?.cache.cache(resource: countries)
+      }
+      completion(result)
+    }
+  }
 }
