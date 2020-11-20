422b40141002fb68bc4cf4cca1d995b5830f0e28
Marko Engelman <engleman.marko@gmail.com>
2020-11-20 21:06:54 +0100

    Inject 'date'.

--- CleanAirModules/Shared/ResourceCache/ResourceCacheLoader.swift
+++ CleanAirModules/Shared/ResourceCache/ResourceCacheLoader.swift
@@ -7,26 +7,28 @@
 
 import Foundation
 
-public class ResourceCacheLoader<Resource, ResourceStorage> where ResourceStorage: Storage, ResourceStorage.StorableObject == Resource {
+public class ResourceCacheLoader<Resource, ResourceStorage> where ResourceStorage: Storage {
   let storage: ResourceStorage
+  let date: () -> Date
   
   enum ResourceCacheError: Swift.Error {
     case storage
   }
   
-  public init(storage: ResourceStorage) {
+  public init(storage: ResourceStorage, date: @escaping () -> Date = Date.init ) {
     self.storage = storage
+    self.date = date
   }
   
-  public func cache(resource: Resource) {
-    storage.store(resource)
+  public func cache(resource: Resource) where ResourceStorage.StorableObject == ResourceCache<Resource> {
+    storage.store(ResourceCache(timestamp: date().timeIntervalSince1970, resource: resource))
   }
   
-  public func load() throws -> Resource {
+  public func load() throws -> Resource where ResourceStorage.StorableObject == ResourceCache<Resource> {
     let loadResult = storage.load()
     switch loadResult {
-    case let .success(resource):
-      return resource
+    case let .success(cache):
+      return cache.resource
       
     case .failure:
       throw ResourceCacheError.storage
