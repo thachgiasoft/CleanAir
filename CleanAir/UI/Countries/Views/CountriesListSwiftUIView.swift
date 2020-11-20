40e994e656c7a33c637aa0b6e73d90bbf14b5dcb
Marko Engelman <engleman.marko@gmail.com>
2020-11-20 10:24:21 +0100

    Add 'CountriesListSwiftUIView'.

---
+++ CleanAir/UI/Countries/Views/CountriesListSwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  CountriesListSwiftUIView.swift
+//  CleanAir
+//
+//  Created by Marko Engelman on 20/11/2020.
+//
+
+import SwiftUI
+
+struct CountriesListSwiftUIView: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+struct CountriesListSwiftUIView_Previews: PreviewProvider {
+  static var previews: some View {
+    CountriesListSwiftUIView()
+  }
+}
