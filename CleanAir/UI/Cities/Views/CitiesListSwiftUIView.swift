99ec2d7481c3afe8bf2eb9ef3da44cd43c266104
Marko Engelman <engleman.marko@gmail.com>
2020-11-20 13:12:00 +0100

    Add 'CitiesListSwiftUIView'.

---
+++ CleanAir/UI/Cities/Views/CitiesListSwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  CitiesListSwiftUIView.swift
+//  CleanAir
+//
+//  Created by Marko Engelman on 20/11/2020.
+//
+
+import SwiftUI
+
+struct CitiesListSwiftUIView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct CitiesListSwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        CitiesListSwiftUIView()
+    }
+}
