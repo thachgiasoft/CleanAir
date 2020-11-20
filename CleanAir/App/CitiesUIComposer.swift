2f366f1f9894f591101f5a91acd0600cb6f8aed9
Marko Engelman <engleman.marko@gmail.com>
2020-11-20 13:35:25 +0100

    Add 'CitiesUIComposer',

---
+++ CleanAir/App/CitiesUIComposer.swift
@@ -0,0 +1,30 @@
+//
+//  CitiesUIComposer.swift
+//  CleanAir
+//
+//  Created by Marko Engelman on 20/11/2020.
+//
+
+import SwiftUI
+
+final class CitiesUIComposer {
+  static func makeView(with loader: CitiesLoader, selection: @escaping (City) -> Void) -> UIViewController {
+    let adapter = ResourcePresentationAdapter<[City], WeakRef<CitiesListViewViewModel>>(loader: loader)
+    let viewModel = ResourceListViewViewModel<City>(
+      onAppear: adapter.load,
+      onSelect: selection,
+      resource: []
+    )
+    
+    var view = CitiesListSwiftUIView(onAppear: { }, viewModel: viewModel)
+    let presenter = ResourcePresenter<[City], WeakRef<CitiesListViewViewModel>>(
+      view: WeakRef(viewModel),
+      loadingView: WeakRef(viewModel),
+      errorView: WeakRef(viewModel)
+    )
+    adapter.presenter = presenter
+    view.onAppear = adapter.load
+    let controller = UIHostingController(rootView: view)
+    return controller
+  }
+}
