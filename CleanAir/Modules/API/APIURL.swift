01255a70bda1a34a4fdccbf5f486f819d168fb2c
Marko Engelman <engleman.marko@gmail.com>
2020-11-20 13:19:02 +0100

    Add 'cities' to 'APIURL'.

--- CleanAir/Modules/API/APIURL.swift
+++ CleanAir/Modules/API/APIURL.swift
@@ -11,4 +11,8 @@ struct APIURL {
   private static let base = "https://api.openaq.org"
   private static let version = "v1"
   static let countries = URL(string: "\(base)/\(version)/countries")!
+  
+  static func cities(for country: String) -> URL {
+    return URL(string: "\(base)/\(version)/cities?country=\(country)")!
+  }
 }
