9a544fc8057c7ab7a2d22c206b90a22846b2a2a3
Marko Engelman <engleman.marko@gmail.com>
2020-11-20 17:04:17 +0100

    Add 'CountriesLoaderWithCaching'.

---
+++ CleanAirModules/Countries/CountriesLoaderWithCaching.swift
@@ -0,0 +1,12 @@
+//
+//  CountriesLoaderWithCaching.swift
+//  CleanAirModules
+//
+//  Created by Marko Engelman on 20/11/2020.
+//
+
+import Foundation
+
+public class CountriesLoaderWithCaching {
+  
+}
