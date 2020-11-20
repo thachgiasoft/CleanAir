ebc73986bccf4caa1b8bed390fe48f27f505ed33
Marko Engelman <engleman.marko@gmail.com>
2020-11-20 11:56:04 +0100

    Show countries.

--- CleanAir/App/AppDelegate.swift
+++ CleanAir/App/AppDelegate.swift
@@ -12,7 +12,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     window = UIWindow(frame: UIScreen.main.bounds)
-    window?.rootViewController = ViewController()
+    let loader = ResourceLoader(
+      client: URLSessionHTTPClient(session: .shared),
+      url: APIURL.countries,
+      mapper: ResourceMapper(CountryMapper.map).map
+    )
+    window?.rootViewController = CountriesUIComposer.makeView(with: loader)
     window?.makeKeyAndVisible()
     return true
   }
