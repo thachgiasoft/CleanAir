6c3178aa2fe3e3b847ef5ea78df53752bcd1a2a0
Marko Engelman <engleman.marko@gmail.com>
2020-11-20 16:20:53 +0100

    Add 'CountriesStorageMapper'.

---
+++ CleanAirModules/CountriesStorage/CountriesStorageMapper.swift
@@ -0,0 +1,30 @@
+//
+//  CountriesStorageMapper.swift
+//  CleanAirModules
+//
+//  Created by Marko Engelman on 20/11/2020.
+//
+
+import Foundation
+import RealmSwift
+
+public final class CountriesStorageMapper {
+  public static func map(_ models: [Country]) -> [RealmCountry] {
+    return models.compactMap { model in
+      let rlmModel = RealmCountry()
+      rlmModel.code = model.code
+      rlmModel.name = model.name
+      rlmModel.count = model.totalNumberOfMeasurements
+      rlmModel.cities = model.numberOfMeasuredCities
+      rlmModel.locations = model.numberOfMeasuringLocations
+      return rlmModel
+    }
+  }
+  
+  public static func map(_ local: Results<RealmCountry>) -> [Country] {
+    return local.compactMap {
+      guard let name = $0.name else { return nil }
+      return Country(code: $0.code, name: name, totalNumberOfMeasurements: $0.count, numberOfMeasuredCities: $0.cities, numberOfMeasuringLocations: $0.locations)
+    }
+  }
+}
