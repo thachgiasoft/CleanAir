3d1e48d5a354e78cff77bdf794db8260741e50e0
Marko Engelman <engleman.marko@gmail.com>
2020-11-20 01:00:11 +0100

    Compose 'CountriesPresenter'.

--- CleanAir/Presentation/Countries/CountriesPresenter.swift
+++ CleanAir/Presentation/Countries/CountriesPresenter.swift
@@ -8,5 +8,31 @@
 import Foundation
 
 class CountriesPresenter {
+  let view: CountriesView
+  let loadingView: CountriesLoadingView
+  let errorView: CountriesErrorView
+  private var isLoading = false
   
+  init(view: CountriesView, loadingView: CountriesLoadingView, errorView: CountriesErrorView) {
+    self.view = view
+    self.loadingView = loadingView
+    self.errorView = errorView
+  }
+  
+  func didStartLoading() {
+    isLoading = true
+    loadingView.show(loadingViewModel: CountriesLoadingViewModel(isLoading: isLoading))
+  }
+  
+  func didFinishLoading(with countries: [Country]) {
+    isLoading = false
+    loadingView.show(loadingViewModel: CountriesLoadingViewModel(isLoading: isLoading))
+    view.show(countriesViewModel: CountriesViewModel(countries: countries))
+  }
+  
+  func didFinishLoading(with error: Error) {
+    isLoading = false
+    loadingView.show(loadingViewModel: CountriesLoadingViewModel(isLoading: isLoading))
+    errorView.show(errorViewModel: CountriesErrorViewModel(error: error.localizedDescription))
+  }
 }
