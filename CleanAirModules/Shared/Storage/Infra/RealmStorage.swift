1d2779afe75f5474d7ad937b46ef9254e869869f
Marko Engelman <engleman.marko@gmail.com>
2020-11-20 16:01:30 +0100

    Compose 'RealmStorage'.

--- CleanAirModules/Shared/Storage/Infra/RealmStorage.swift
+++ CleanAirModules/Shared/Storage/Infra/RealmStorage.swift
@@ -6,7 +6,40 @@
 //
 
 import Foundation
+import RealmSwift
 
-class RealmStorage {
+public class RealmStorage<StoringObject, RealmObject> where RealmObject: Object {
+  let storeMapper: (StoringObject) -> [RealmObject]
+  let loadMapper: (Results<RealmObject>) -> StoringObject
   
+  public init(storeMapper: @escaping (StoringObject) -> [RealmObject], loadMapper: @escaping (Results<RealmObject>) -> StoringObject) {
+    self.storeMapper = storeMapper
+    self.loadMapper = loadMapper
+  }
+}
+
+// MARK: - Storage
+extension RealmStorage: Storage {
+  public func store(_ object: StoringObject) -> Storage.StoreResult {
+    do {
+      let realm = try Realm()
+      try realm.write {
+        realm.add(storeMapper(object), update: .modified)
+      }
+      return .success(())
+    } catch {
+      return .failure(error)
+    }
+  }
+  
+  public func load() -> Swift.Result<StoringObject, Error> {
+    do {
+      let realm = try Realm()
+      let result = realm.objects(RealmObject.self)
+      let loadResult = loadMapper(result)
+      return .success(loadResult)
+    } catch {
+      return .failure(error)
+    }
+  }
 }
