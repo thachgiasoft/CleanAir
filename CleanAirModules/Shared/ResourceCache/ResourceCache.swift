57ac501712ceceb5bcd6591ea144806b9b88f97e
Marko Engelman <engleman.marko@gmail.com>
2020-11-20 20:02:49 +0100

    Add 'ResourceCache'.

---
+++ CleanAirModules/Shared/ResourceCache/ResourceCache.swift
@@ -0,0 +1,13 @@
+//
+//  ResourceCache.swift
+//  CleanAirModules
+//
+//  Created by Marko Engelman on 20/11/2020.
+//
+
+import Foundation
+
+public struct ResourceCache<Resource> {
+  public let timestamp: TimeInterval
+  public let resource: Resource
+}
