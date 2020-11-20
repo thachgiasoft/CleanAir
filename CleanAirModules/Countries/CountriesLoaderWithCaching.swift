5f53406c167d78a5af7f5f8f897fdfc921c704fa
Marko Engelman <engleman.marko@gmail.com>
2020-11-20 21:21:45 +0100

    Load from cache.

--- CleanAirModules/Countries/CountriesLoaderWithCaching.swift
+++ CleanAirModules/Countries/CountriesLoaderWithCaching.swift
@@ -20,6 +20,18 @@ public class CountriesLoaderWithCaching {
 // MARK: - CountriesLoader
 extension CountriesLoaderWithCaching: CountriesLoader {
   public func load(completion: @escaping (Result<[Country], Error>) -> Void) {
+    do {
+      let cacheResult = try cache.load()
+      completion(.success(cacheResult))
+    } catch {
+      fetch(completion: completion)
+    }
+  }
+}
+
+// MARK: - Private
+private extension CountriesLoaderWithCaching {
+  func fetch(completion: @escaping (Result<[Country], Error>) -> Void) {
     loader.load { [weak self] result in
       if case let .success(countries) = result {
         self?.cache.cache(resource: countries)
