7aae786524c44af050d3ebf0af70caa0cce3024c
Marko Engelman <engleman.marko@gmail.com>
2020-11-20 21:01:48 +0100

    Update mapping.

--- CleanAirModules/Countries/CountriesStorage/CountriesStorageMapper.swift
+++ CleanAirModules/Countries/CountriesStorage/CountriesStorageMapper.swift
@@ -9,6 +9,10 @@ import Foundation
 import RealmSwift
 
 public final class CountriesStorageMapper {
+  public enum CacheError: Swift.Error {
+    case invalidCacheResult
+  }
+  
   public static func map(_ models: [Country]) -> [LocalCountry] {
     return models.map(CountriesStorageMapper.map)
   }
@@ -17,6 +21,21 @@ public final class CountriesStorageMapper {
     return local.map {
       return Country(code: $0.code, name: $0.name, totalNumberOfMeasurements: $0.count, numberOfMeasuredCities: $0.cities, numberOfMeasuringLocations: $0.locations)
     }
+  }
+  
+  public static func map(_ cache: ResourceCache<[Country]>) -> LocalCountryCache {
+    let localCache = LocalCountryCache()
+    localCache.timeStamp = cache.timestamp
+    cache.resource.forEach { localCache.countries.append(CountriesStorageMapper.map($0)) }
+    return localCache
+  }
+  
+  public static func map(_ localCache: Results<LocalCountryCache>) throws -> ResourceCache<[Country]> {
+    guard let cache = localCache.first, localCache.count == 1 else { throw CacheError.invalidCacheResult }
+    return ResourceCache(
+      timestamp: cache.timeStamp,
+      resource: cache.countries.map(CountriesStorageMapper.map)
+    )
   }
 }
 
@@ -30,5 +49,15 @@ private extension CountriesStorageMapper {
     rlmModel.cities = model.numberOfMeasuredCities
     rlmModel.locations = model.numberOfMeasuringLocations
     return rlmModel
+  }
+  
+  static func map(_ local: LocalCountry) -> Country {
+    return Country(
+      code: local.code,
+      name: local.name,
+      totalNumberOfMeasurements: local.count,
+      numberOfMeasuredCities: local.cities,
+      numberOfMeasuringLocations: local.locations
+    )
   }
 }
