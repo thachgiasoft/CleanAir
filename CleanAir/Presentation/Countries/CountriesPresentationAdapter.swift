1213c81e5ef00b90813ecb548b8efb53452bfc2f
Marko Engelman <engleman.marko@gmail.com>
2020-11-20 01:19:22 +0100

    Compose 'CountriesPresentationAdapter'.

--- CleanAir/Presentation/Countries/CountriesPresentationAdapter.swift
+++ CleanAir/Presentation/Countries/CountriesPresentationAdapter.swift
@@ -8,5 +8,25 @@
 import Foundation
 
 class CountriesPresentationAdapter {
+  let loader: CountriesLoader
+  var presenter: CountriesPresenter?
   
+  init(loader: CountriesLoader) {
+    self.loader = loader
+  }
+  
+  func load() {
+    presenter?.didStartLoading()
+    loader.load { [weak self] result in
+      DispatchQueue.main.async {
+        switch result {
+        case let .success(resource):
+          self?.presenter?.didFinishLoading(with: resource)
+          
+        case let .failure(error):
+          self?.presenter?.didFinishLoading(with: error)
+        }
+      }
+    }
+  }
 }
