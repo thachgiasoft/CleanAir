ea8ce2baa4fb4926e4842c3bf6edb3f1ac7adf78
Marko Engelman <engleman.marko@gmail.com>
2020-11-20 17:00:13 +0100

    Compose 'ResourceCache'.

--- CleanAirModules/Shared/ResourceCache/ResourceCache.swift
+++ CleanAirModules/Shared/ResourceCache/ResourceCache.swift
@@ -7,6 +7,29 @@
 
 import Foundation
 
-class ResourceCache<Resource> {
+public class ResourceCache<Resource, ResourceStorage> where ResourceStorage: Storage, ResourceStorage.StorableObject == Resource {
+  let storage: ResourceStorage
   
+  enum ResourceCacheError: Swift.Error {
+    case storage
+  }
+  
+  public init(storage: ResourceStorage) {
+    self.storage = storage
+  }
+  
+  public func cache(resource: Resource) {
+    storage.store(resource)
+  }
+  
+  public func load() throws -> Resource {
+    let loadResult = storage.load()
+    switch loadResult {
+    case let .success(resource):
+      return resource
+      
+    case .failure:
+      throw ResourceCacheError.storage
+    }
+  }
 }
