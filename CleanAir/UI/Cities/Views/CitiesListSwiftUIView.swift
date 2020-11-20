e84f211b816dcd1e72247504530c9a2e74ef87ae
Marko Engelman <engleman.marko@gmail.com>
2020-11-20 13:17:46 +0100

    Compose 'CitiesListSwiftUIView' prototype.

--- CleanAir/UI/Cities/Views/CitiesListSwiftUIView.swift
+++ CleanAir/UI/Cities/Views/CitiesListSwiftUIView.swift
@@ -8,13 +8,51 @@
 import SwiftUI
 
 struct CitiesListSwiftUIView: View {
-    var body: some View {
-        Text("Hello, World!")
+  var onAppear: () -> Void
+  @ObservedObject var viewModel: CitiesListViewViewModel
+  
+  var body: some View {
+    NavigationView {
+      if let error = viewModel.error {
+        Text(error)
+      } else {
+        List(viewModel.resource, id: \.self.name) { city in
+          VStack(alignment: .leading, spacing: 10, content: {
+            Text(city.name)
+              .font(.body)
+          })
+        }.onAppear(perform: {
+          onAppear()
+        })
+        .navigationBarTitle(Text(viewModel.isLoading ? "Loading cities" : "Cities"))
+      }
     }
+  }
 }
 
 struct CitiesListSwiftUIView_Previews: PreviewProvider {
-    static var previews: some View {
-        CitiesListSwiftUIView()
-    }
+  static var previews: some View {
+    let cities = [
+      City(
+        name: "Short name city",
+        country: "country",
+        measurementsCount: 1, availableLocationsCount: 1),
+      City(
+        name: "Name city",
+        country: "country",
+        measurementsCount: 1, availableLocationsCount: 1),
+      City(
+        name: "Long name city",
+        country: "country",
+        measurementsCount: 1, availableLocationsCount: 1),
+      City(
+        name: "Long long name city",
+        country: "country",
+        measurementsCount: 1, availableLocationsCount: 1)
+    ]
+    CitiesListSwiftUIView(
+      onAppear: { },
+      viewModel: ResourceListViewViewModel(onAppear: { }, resource: cities)
+    )
+  }
 }
