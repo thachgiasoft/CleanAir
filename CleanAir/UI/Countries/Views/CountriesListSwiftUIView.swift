353a15a7c31ed64e61105b22e460e1605c6c5a89
Marko Engelman <engleman.marko@gmail.com>
2020-11-20 10:42:25 +0100

    Compose 'CountriesListSwiftUIView'.

--- CleanAir/UI/Countries/Views/CountriesListSwiftUIView.swift
+++ CleanAir/UI/Countries/Views/CountriesListSwiftUIView.swift
@@ -8,13 +8,56 @@
 import SwiftUI
 
 struct CountriesListSwiftUIView: View {
+  var countries: [Country] = []
+  
   var body: some View {
-    Text("Hello, World!")
+    List(countries, id: \.self.name) { country in
+      VStack(alignment: .leading, spacing: 10, content: {
+        Text(country.name)
+          .font(.title)
+        Text("Cities: \(country.numberOfMeasuredCities)")
+          .font(.body)
+        Text("Mesausrements: \(country.numberOfMeasuringLocations)")
+          .font(.body)
+      })
+    }
   }
 }
 
 struct CountriesListSwiftUIView_Previews: PreviewProvider {
   static var previews: some View {
-    CountriesListSwiftUIView()
+    let countries = [
+      Country(
+        name: "Short name country",
+        totalNumberOfMeasurements: 1,
+        numberOfMeasuredCities: 1,
+        numberOfMeasuringLocations: 1),
+      Country(
+        name: "Country",
+        totalNumberOfMeasurements: 1,
+        numberOfMeasuredCities: 1,
+        numberOfMeasuringLocations: 1),
+      Country(
+        name: "Long long long name counrty",
+        totalNumberOfMeasurements: 1,
+        numberOfMeasuredCities: 1,
+        numberOfMeasuringLocations: 1),
+      Country(
+        name: "Short name country 2",
+        totalNumberOfMeasurements: 1,
+        numberOfMeasuredCities: 1,
+        numberOfMeasuringLocations: 1),
+      Country(
+        name: "Country 2",
+        totalNumberOfMeasurements: 1,
+        numberOfMeasuredCities: 1,
+        numberOfMeasuringLocations: 1),
+      Country(
+        name: "Long long long name counrty 2",
+        totalNumberOfMeasurements: 1,
+        numberOfMeasuredCities: 1,
+        numberOfMeasuringLocations: 1)
+    ]
+    CountriesListSwiftUIView(countries: countries)
   }
 }
