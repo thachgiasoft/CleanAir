0d95c36751b23904ac917b71a62151845f334966
Marko Engelman <engleman.marko@gmail.com>
2020-11-21 14:27:51 +0100

    Compose 'CitiesLoaderWithLocalValidation'.

--- CleanAirModules/Cities/CitiesLoader/CitiesLoaderWithLocalValidation.swift
+++ CleanAirModules/Cities/CitiesLoader/CitiesLoaderWithLocalValidation.swift
@@ -20,6 +20,31 @@ public class CitiesLoaderWithLocalValidation {
 // MARK: - CitiesLoader
 extension CitiesLoaderWithLocalValidation: CitiesLoader {
   public func load(completion: @escaping (Result<[City], Error>) -> Void) {
-    
+    loader.load { [weak self] result in
+      guard let self = self else { return }
+      switch result {
+      case let .success(cities):
+        completion(.success(self.validate(cities)))
+        
+      case let .failure(error):
+        completion(.failure(error))
+      }
+    }
+  }
+}
+
+// MARK: - Private
+private extension CitiesLoaderWithLocalValidation {
+  func validate(_ cities: [City]) -> [City] {
+    var validatedCities: [City] = []
+    cities.forEach { city in
+      if let localCity = try? storage.load(objectId: city.id).get(), localCity.isFavourite != city.isFavourite {
+        let updatedCity = City(name: city.name, country: city.country, measurementsCount: city.measurementsCount, availableLocationsCount: city.availableLocationsCount, isFavourite: localCity.isFavourite)
+        validatedCities.append(updatedCity)
+      } else {
+        validatedCities.append(city)
+      }
+    }
+    return validatedCities
   }
 }
