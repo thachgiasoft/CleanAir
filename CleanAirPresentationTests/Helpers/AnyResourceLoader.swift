e886e40803ad2e873e82120b144e54e8939062ac
Marko Engelman <engleman.marko@gmail.com>
2020-11-21 17:36:19 +0100

    Add 'AnyResourceLoader' helper class.

---
+++ CleanAirPresentationTests/Helpers/AnyResourceLoader.swift
@@ -0,0 +1,14 @@
+//
+//  AnyResourceLoader.swift
+//  CleanAirPresentationTests
+//
+//  Created by Marko Engelman on 21/11/2020.
+//
+
+import Foundation
+
+class AnyResourceLoader<T> where T: Any {
+  func load(completion: @escaping (Swift.Result<T, Error>) -> Void) {
+    
+  }
+}
