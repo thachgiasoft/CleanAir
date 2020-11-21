e541d058789f6fa6e6b258fd547051808b7282e4
Marko Engelman <engleman.marko@gmail.com>
2020-11-21 11:35:41 +0100

    Use new components to implement switching favourites.

--- CleanAir/App_iOS/CitiesUIComposer.swift
+++ CleanAir/App_iOS/CitiesUIComposer.swift
@@ -15,7 +15,7 @@ final class CitiesUIComposer {
     let viewModel = CitiesListViewViewModel(
       onAppear: adapter.load,
       onSelect: selection,
-      mapper: CityViewModelMapper.map,
+      mapper: { CitiesUIComposer.viewModel(for: $0, with: service) },
       resource: []
     )
     
@@ -29,5 +29,21 @@ final class CitiesUIComposer {
     view.onAppear = adapter.load
     let controller = UIHostingController(rootView: view)
     return controller
+  }
+}
+
+// MARK: - Private
+private extension CitiesUIComposer {
+  static func viewModel(for city: City, with service: FavouriteCityService) -> CityViewModel {
+    let viewModel = CityViewModelMapper.map(model: city)
+    let adapter = CityPresentationAdapter<WeakRef<CityViewModel>>(city: city, service: service)
+    adapter.presenter = ResourcePresenter(
+      view: WeakRef(viewModel),
+      loadingView: WeakRef(viewModel),
+      errorView: WeakRef(viewModel),
+      viewModelMapper: CityViewModelMapper.map
+    )
+    viewModel.togglFavourite = adapter.togglFavourite
+    return viewModel
   }
 }
