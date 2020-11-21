64a0c9546679979cd3f2ba74281111e1663e7c05
Marko Engelman <engleman.marko@gmail.com>
2020-11-21 17:41:07 +0100

    Add 'makeSUT'.

--- CleanAirPresentationTests/CleanAirPresentationTests.swift
+++ CleanAirPresentationTests/CleanAirPresentationTests.swift
@@ -6,7 +6,23 @@
 //
 
 import XCTest
+@testable import CleanAirPresentation
 
 class CleanAirPresentationTests: XCTestCase {
+}
 
+// MARK: - Private
+private extension CleanAirPresentationTests {
+  typealias AnyType = String
+  typealias AnyView = AnyResourceView<AnyType>
+  typealias AnyLoader = AnyResourceLoader<AnyType>
+  typealias AnyPresenter = ResourcePresenter<AnyType, AnyView>
+  typealias AnyPresentationAdapter = ResourcePresentationAdapter<AnyType, AnyView>
+  
+  func makeSUT() -> AnyPresentationAdapter {
+    let view = AnyView()
+    let sut = AnyPresentationAdapter(loader: AnyLoader().load)
+    sut.presenter = AnyPresenter(view: view, loadingView: view, errorView: view)
+    return sut
+  }
 }
