2b020ef2e30905801a8159c6b2bdd433527a862a
Marko Engelman <engleman.marko@gmail.com>
2020-11-21 23:27:45 +0100

    Init stores injected session.

--- CleanAirModulesTests/HTTPClient/HTTPClientTests.swift
+++ CleanAirModulesTests/HTTPClient/HTTPClientTests.swift
@@ -6,7 +6,20 @@
 //
 
 import XCTest
+@testable import CleanAirModules
 
 class HTTPClientTests: XCTestCase {
-  
+  func test_init_storesSession() {
+    let configuration = URLSessionConfiguration.ephemeral
+    let session = URLSession(configuration: configuration)
+    let sut = makeSUT(session: session)
+    XCTAssertEqual(sut.session, session)
+  }
+}
+
+// MARK: - Private
+private extension HTTPClientTests {
+  func makeSUT(session: URLSession = .shared) -> URLSessionHTTPClient {
+    return URLSessionHTTPClient(session: session)
+  }
 }
