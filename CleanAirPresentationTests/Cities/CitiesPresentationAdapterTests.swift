38828f2f43fb710c1c56fa6318c3bd5c849d288f
Marko Engelman <engleman.marko@gmail.com>
2020-11-21 22:12:11 +0100

    Add 'CitiesPresentationAdapterTests'.

---
+++ CleanAirPresentationTests/Cities/CitiesPresentationAdapterTests.swift
@@ -0,0 +1,12 @@
+//
+//  CitiesPresentationAdapterTests.swift
+//  CleanAirPresentationTests
+//
+//  Created by Marko Engelman on 21/11/2020.
+//
+
+import XCTest
+
+class CitiesPresentationAdapterTests: XCTestCase {
+  
+}
