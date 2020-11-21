adfb102ee13acb0ebdc5d7a72317544aecdcba50
Marko Engelman <engleman.marko@gmail.com>
2020-11-21 19:09:15 +0100

    Add 'ResourcePresenterTests'.

---
+++ CleanAirPresentationTests/ResourcePresenterTests.swift
@@ -0,0 +1,12 @@
+//
+//  ResourcePresenterTests.swift
+//  CleanAirPresentationTests
+//
+//  Created by Marko Engelman on 21/11/2020.
+//
+
+import XCTest
+
+class ResourcePresenterTests: XCTestCase {
+  
+}
