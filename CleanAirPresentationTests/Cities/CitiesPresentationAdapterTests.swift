68a07cda88c300355cb05d4c729665a3a6bc2ab6
Marko Engelman <engleman.marko@gmail.com>
2020-11-21 22:33:11 +0100

    Init doesn't trigger any calls.

--- CleanAirPresentationTests/Cities/CitiesPresentationAdapterTests.swift
+++ CleanAirPresentationTests/Cities/CitiesPresentationAdapterTests.swift
@@ -6,7 +6,29 @@
 //
 
 import XCTest
+@testable import CleanAirPresentation
+@testable import CleanAirModules
 
 class CitiesPresentationAdapterTests: XCTestCase {
+  func test_init_doesntTriggerService() {
+    let (_, _, service) = makeSUT(city: anyCity())
+    XCTAssertEqual(service.callCount, 0)
+  }
+}
+
+// MARK: - Private
+private extension CitiesPresentationAdapterTests {
+  typealias View = AnyResourceView<City>
+  typealias Presenter = ResourcePresenter<City, View>
+  typealias Adapter = CityPresentationAdapter<View>
+  typealias Service = FavouriteCityServiceMock
   
+  func makeSUT(city: City) -> (Adapter, Presenter, Service) {
+    let view = View()
+    let service = Service()
+    let sut = Adapter(city: city, service: service)
+    let presenter = Presenter(view: view, loadingView: view, errorView: view)
+    sut.presenter = presenter
+    return (sut, presenter, service)
+  }
 }
