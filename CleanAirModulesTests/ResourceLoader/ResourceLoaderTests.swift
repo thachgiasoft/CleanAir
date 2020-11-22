4183decb04e37e69be017374172994fe717a877d
Marko Engelman <engleman.marko@gmail.com>
2020-11-22 20:10:42 +0100

    Add 'makeSUT'.

--- CleanAirModulesTests/ResourceLoader/ResourceLoaderTests.swift
+++ CleanAirModulesTests/ResourceLoader/ResourceLoaderTests.swift
@@ -6,7 +6,37 @@
 //
 
 import XCTest
+@testable import CleanAirModules
 
 class ResourceLoaderTests: XCTestCase {
 
 }
+
+// MARK: - Private
+private extension ResourceLoaderTests {
+  typealias AnyResource = String
+  typealias AnyResourceLoder = ResourceLoader<AnyResource>
+  
+  func makeSUT() -> AnyResourceLoder {
+    let url = URL(string: "https://www.anyURL.com")!
+    let client = HTTPClientStub()
+    let loader = AnyResourceLoder(
+      client: client,
+      url: url,
+      mapper: ResourceResultsMapper({ $0 }).map
+    )
+    return loader
+  }
+  
+  class HTTPClientStub: HTTPClient {
+    class HTTPClientTaskMock: HTTPClientTask {
+      func cancel() { }
+    }
+    
+    func execute(request: URLRequest, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+      return HTTPClientTaskMock()
+    }
+  }
+}
+
+
