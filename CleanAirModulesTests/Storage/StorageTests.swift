3c8e9a7880f4148534c4dc4caceb29418069e032
Marko Engelman <engleman.marko@gmail.com>
2020-11-22 09:46:34 +0100

    Add 'makeSUT'.

--- CleanAirModulesTests/Storage/StorageTests.swift
+++ CleanAirModulesTests/Storage/StorageTests.swift
@@ -6,7 +6,30 @@
 //
 
 import XCTest
+@testable import CleanAirModules
+@testable import RealmSwift
 
 class StorageTests: XCTestCase {
   
+}
+
+// MARK: - Private
+private extension StorageTests {
+  class AnyLocalType: Object {
+    @objc dynamic var id: AnyType = 0
+    override class func primaryKey() -> String? {
+      return "id"
+    }
+  }
+  typealias AnyType = Int
+  
+  func makeSUT() -> RealmStorage<AnyType, AnyLocalType> {
+    let realm = try! Realm()
+    let sut = RealmStorage(
+      realm: realm,
+      storeMapper: { AnyLocalType(value: $0) },
+      objectMapper: { $0.id }
+    )
+    return sut
+  }
 }
