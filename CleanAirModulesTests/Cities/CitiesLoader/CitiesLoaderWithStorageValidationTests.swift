5a9300be89c6778ae4fdb50b8d3ee4461a55c1cd
Marko Engelman <engleman.marko@gmail.com>
2020-11-23 11:59:54 +0100

    Add 'CitiesLoaderWithStorageValidationTests'.

---
+++ CleanAirModulesTests/Cities/CitiesLoader/CitiesLoaderWithStorageValidationTests.swift
@@ -0,0 +1,12 @@
+//
+//  CitiesLoaderWithStorageValidationTests.swift
+//  CleanAirModulesTests
+//
+//  Created by Marko Engelman on 23/11/2020.
+//
+
+import XCTest
+
+class CitiesLoaderWithStorageValidationTests: XCTestCase {
+  
+}
