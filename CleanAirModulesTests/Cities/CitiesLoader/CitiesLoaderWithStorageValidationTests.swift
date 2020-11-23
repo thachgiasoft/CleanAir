40b5c8e1bf46ec26f3d2e302a4dce38f41b1b2aa
Marko Engelman <engleman.marko@gmail.com>
2020-11-23 16:34:30 +0100

    Load has no side effects on storage when loader fails.

--- CleanAirModulesTests/Cities/CitiesLoader/CitiesLoaderWithStorageValidationTests.swift
+++ CleanAirModulesTests/Cities/CitiesLoader/CitiesLoaderWithStorageValidationTests.swift
@@ -14,6 +14,28 @@ class CitiesLoaderWithStorageValidationTests: XCTestCase {
     XCTAssertNil(loader.completion)
     XCTAssertTrue(storage.stored.isEmpty)
   }
+  
+  func test_load_eventuallyDeliversResult() {
+    let (sut, loader, _) = makeSUT()
+    let exp = expectation(description: "Waiting for deliver")
+    sut.load { _ in
+      exp.fulfill()
+    }
+    loader.complete(with: anyCity())
+    wait(for: [exp], timeout: 1.0)
+  }
+  
+  func test_load_hasNoSideEffectOnStore_onLoaderError() {
+    let (sut, loader, store) = makeSUT()
+    let exp = expectation(description: "Waiting for deliver")
+    sut.load { _ in
+      exp.fulfill()
+    }
+    loader.completeWithError()
+    wait(for: [exp], timeout: 1.0)
+    XCTAssertEqual(store.storeCalls, .zero)
+    XCTAssertEqual(store.removeCalls, .zero)
+  }
 }
 
 // MARK: - Private
@@ -38,12 +60,19 @@ private extension CitiesLoaderWithStorageValidationTests {
     func complete(with city: City) {
       completion?(.success([city]))
     }
+    
+    func completeWithError() {
+      completion?(.failure(NSError(domain: "", code: 1, userInfo: [:])))
+    }
   }
   
   class CityStorageMock: CityStorage {
     var stored: [String: City] = [:]
+    var removeCalls: Int = 0
+    var storeCalls: Int = 0
     
     func store(_ object: City, completion: @escaping (StoreResult) -> Void) {
+      storeCalls += 1
       stored[object.id] = object
     }
     
@@ -56,6 +85,7 @@ private extension CitiesLoaderWithStorageValidationTests {
     }
     
     func remove(objectId: Any, completion: @escaping (RemoveResult) -> Void) {
+      removeCalls += 1
       stored.removeValue(forKey: objectId as! String)
       completion(.success(()))
     }
