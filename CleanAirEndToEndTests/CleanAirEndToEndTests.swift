01b5beebcd7d0c6c1b0d2f546e7a1d41c9b5a4e2
Marko Engelman <engleman.marko@gmail.com>
2020-11-24 20:05:32 +0100

    Add 'CleanAirEndToEndTests'.

---
+++ CleanAirEndToEndTests/CleanAirEndToEndTests.swift
@@ -0,0 +1,12 @@
+//
+//  CleanAirEndToEndTests.swift
+//  CleanAirEndToEndTests
+//
+//  Created by Marko Engelman on 24/11/2020.
+//
+
+import XCTest
+
+class CleanAirEndToEndTests: XCTestCase {
+  
+}
