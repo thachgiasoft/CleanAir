da5ed963a4ebd9ab56ce63136154dc52487c229f
Marko Engelman <engleman.marko@gmail.com>
2020-11-25 14:30:51 +0100

    Add 'RealmCuntriesCacheStorageTests'.

---
+++ CleanAirModulesTests/Countries/RealmCuntriesCacheStorageTests.swift
@@ -0,0 +1,12 @@
+//
+//  RealmCuntriesCacheStorageTests.swift
+//  CleanAirModulesTests
+//
+//  Created by Marko Engelman on 25/11/2020.
+//
+
+import XCTest
+
+class RealmCuntriesCacheStorageTests: XCTestCase {
+  
+}
