1b1a38936898129c7b30c29cfa86b2912147cd7c
Marko Engelman <engleman.marko@gmail.com>
2020-11-25 14:16:55 +0100

    Add 'RealmCitiesStorageTests'.

---
+++ CleanAirModulesTests/Cities/CitiesStorage/RealmCitiesStorageTests.swift
@@ -0,0 +1,12 @@
+//
+//  RealmCitiesStorageTests.swift
+//  CleanAirModulesTests
+//
+//  Created by Marko Engelman on 25/11/2020.
+//
+
+import XCTest
+
+class RealmCitiesStorageTests: XCTestCase {
+  
+}
