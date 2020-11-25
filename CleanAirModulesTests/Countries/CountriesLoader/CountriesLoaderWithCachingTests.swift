93a01efed06387bc42025d2a2292289d9ca4dde5
Marko Engelman <engleman.marko@gmail.com>
2020-11-25 14:56:27 +0100

    Add 'CountriesLoaderWithCachingTests'.

---
+++ CleanAirModulesTests/Countries/CountriesLoader/CountriesLoaderWithCachingTests.swift
@@ -0,0 +1,12 @@
+//
+//  CountriesLoaderWithCachingTests.swift
+//  CleanAirModulesTests
+//
+//  Created by Marko Engelman on 25/11/2020.
+//
+
+import XCTest
+
+class CountriesLoaderWithCachingTests: XCTestCase {
+  
+}
