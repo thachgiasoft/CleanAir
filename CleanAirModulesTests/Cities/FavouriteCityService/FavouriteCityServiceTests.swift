76fd0253b5b7ba324acd61ab3dcb74c20f851fdb
Marko Engelman <engleman.marko@gmail.com>
2020-11-25 16:16:52 +0100

    Add 'FavouriteCityServiceTests'.

---
+++ CleanAirModulesTests/Cities/FavouriteCityService/FavouriteCityServiceTests.swift
@@ -0,0 +1,12 @@
+//
+//  FavouriteCityServiceTests.swift
+//  CleanAirModulesTests
+//
+//  Created by Marko Engelman on 25/11/2020.
+//
+
+import XCTest
+
+class FavouriteCityServiceTests: XCTestCase {
+  
+}
