e6a74458da2476d84daf05c38326abc9ba809b34
Marko Engelman <engleman.marko@gmail.com>
2020-11-25 22:21:51 +0100

    Remove 'NavigationView' from 'CountriesListSwiftUIView'.

--- CleanAir/UI/Countries/Views/CountriesListSwiftUIView.swift
+++ CleanAir/UI/Countries/Views/CountriesListSwiftUIView.swift
@@ -12,26 +12,24 @@ struct CountriesListSwiftUIView: View {
   @ObservedObject var viewModel: CountriesListViewViewModel
   
   var body: some View {
-    NavigationView {
-      if let error = viewModel.error {
-        Text(error)
-      } else {
-        List(viewModel.resource, id: \.self.name) { country in
-          VStack(alignment: .leading, spacing: 10, content: {
-            Text(country.name)
-              .font(.title)
-            Text("Cities: \(country.numberOfMeasuredCities)")
-              .font(.body)
-            Text("Mesausrements: \(country.numberOfMeasuringLocations)")
-              .font(.body)
-          }).onTapGesture {
-            viewModel.selection(country)
-          }
-        }.onAppear(perform: {
-          onAppear()
-        })
-        .navigationBarTitle(Text(viewModel.isLoading ? "Loading countries" : "Countries"))
-      }
+    if let error = viewModel.error {
+      Text(error)
+    } else {
+      List(viewModel.resource, id: \.self.name) { country in
+        VStack(alignment: .leading, spacing: 10, content: {
+          Text(country.name)
+            .font(.title)
+          Text("Cities: \(country.numberOfMeasuredCities)")
+            .font(.body)
+          Text("Mesausrements: \(country.numberOfMeasuringLocations)")
+            .font(.body)
+        }).onTapGesture {
+          viewModel.selection(country)
+        }
+      } .onAppear(perform: {
+        onAppear()
+      })
+      .navigationBarTitle(Text(viewModel.isLoading ? "Loading countries" : "Countries"))
     }
   }
 }
