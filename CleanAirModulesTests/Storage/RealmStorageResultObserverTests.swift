4a7143967e6e248d5f22be1707edaa5b2d168408
Marko Engelman <engleman.marko@gmail.com>
2020-11-26 18:22:53 +0100

    Add 'RealmStorageResultObserverTests'.

---
+++ CleanAirModulesTests/Storage/RealmStorageResultObserverTests.swift
@@ -0,0 +1,12 @@
+//
+//  RealmStorageResultObserverTests.swift
+//  CleanAirModulesTests
+//
+//  Created by Marko Engelman on 26/11/2020.
+//
+
+import XCTest
+
+class RealmStorageResultObserverTests: XCTestCase {
+
+}
