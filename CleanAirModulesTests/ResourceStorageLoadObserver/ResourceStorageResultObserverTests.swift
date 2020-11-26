dfee806a467152280acf91d1093dcdde239f5432
Marko Engelman <engleman.marko@gmail.com>
2020-11-26 20:33:11 +0100

    Add 'ResourceStorageResultObserverTests'.

---
+++ CleanAirModulesTests/ResourceStorageLoadObserver/ResourceStorageResultObserverTests.swift
@@ -0,0 +1,12 @@
+//
+//  ResourceStorageResultObserverTests.swift
+//  CleanAirModulesTests
+//
+//  Created by Marko Engelman on 26/11/2020.
+//
+
+import XCTest
+
+class ResourceStorageResultObserverTests: XCTestCase {
+
+}
