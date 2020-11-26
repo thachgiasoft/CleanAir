c083dbb0f2e5d7e64ac9c0b26ed51f4efa635661
Marko Engelman <engleman.marko@gmail.com>
2020-11-26 13:44:47 +0100

    Add 'CitiesStorageLoadRequest'.

--- CleanAirModules/Cities/CitiesStorage/CitiesStorage.swift
+++ CleanAirModules/Cities/CitiesStorage/CitiesStorage.swift
@@ -7,6 +7,14 @@
 
 import Foundation
 
+public struct CityStorageLoadRequest {
+  let isFavourite: Bool
+  
+  public init(isFavourite: Bool) {
+    self.isFavourite = isFavourite
+  }
+}
+
 public protocol CityStorage {
   func store(_ city: City) throws
   func remove(cityId: String) throws
