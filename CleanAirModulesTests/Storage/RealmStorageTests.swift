8bc8b4cf786f8ab6e9b7c17f0abd000939c0bed8
Marko Engelman <engleman.marko@gmail.com>
2020-11-26 14:05:16 +0100

    Add option to query with NSPredicate from storage.

--- CleanAirModulesTests/Storage/RealmStorageTests.swift
+++ CleanAirModulesTests/Storage/RealmStorageTests.swift
@@ -13,38 +13,56 @@ class RealmStorageTests: XCTestCase {
   override class func setUp() {
     preapareForTesting()
   }
-
+  
   func test_insert_doesntThrowError() {
     let sut = makeSUT()
     XCTAssertNoThrow(try sut.insert(object: anyLocal))
   }
-
+  
   func test_find_deliversEmptyOnEmptyStore() {
     let sut = makeSUT()
     XCTAssertTrue(sut.find(object: type(of: anyLocal)).isEmpty)
   }
-
+  
   func test_load_deliversStoredResult() throws {
     let local = anyLocal
     let sut = makeSUT()
     try sut.insert(object: local)
     XCTAssertTrue(sut.find(object: type(of: local)).first == local)
   }
-
+  
+  func test_load_deliversQueriedResult() throws {
+    let local = anyLocal
+    let sut = makeSUT()
+    try sut.insert(object: local)
+    let existingValues = sut
+      .find(object: type(of: local),
+            filtered: NSPredicate(format: "queryValue = %@", local.queryValue)
+      )
+    
+    let nonExistingValues = sut
+      .find(object: type(of: local),
+            filtered: NSPredicate(format: "queryValue = %@", anyLocal.queryValue)
+      )
+    
+    XCTAssertFalse(existingValues.isEmpty)
+    XCTAssertTrue(nonExistingValues.isEmpty)
+  }
+  
   func test_loadForId_deliversStoredResult() throws {
     let local = anyLocal
     let sut = makeSUT()
     try sut.insert(object: local)
     XCTAssertEqual(sut.find(object: type(of: local), forId: local.id), local)
   }
-
+  
   func test_removeExistingObject_doesntThrowError() throws {
     let local = anyLocal
     let sut = makeSUT()
     try sut.insert(object: local)
     XCTAssertNoThrow(try sut.delete(object: type(of: local), forId: local.id))
   }
-
+  
   func test_removeUnexistingObject_ThrowsError() throws {
     let local = anyLocal
     let sut = makeSUT()
@@ -64,9 +82,10 @@ private extension RealmStorageTests {
   }
   
   static func local(for value: AnyType) -> AnyLocalType {
-      let local = AnyLocalType()
-      local.id = value
-      return local
+    let local = AnyLocalType()
+    local.id = value
+    local.queryValue = value
+    return local
   }
   
   var anyLocal: AnyLocalType {
@@ -76,6 +95,8 @@ private extension RealmStorageTests {
 
 class AnyLocalType: Object {
   @objc dynamic var id = ""
+  @objc dynamic var queryValue = ""
+  
   override class func primaryKey() -> String? {
     return "id"
   }
