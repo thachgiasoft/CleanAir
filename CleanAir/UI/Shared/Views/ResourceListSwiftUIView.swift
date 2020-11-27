8ee87cbb816fec68b54683ce338af0c123979ced
Marko Engelman <engleman.marko@gmail.com>
2020-11-27 19:37:49 +0100

    Add shared 'ResourceLoadingListSwiftUIView', 'ResourceListSwiftUIView' and 'ResourceSwiftUIView'.

---
+++ CleanAir/UI/Shared/Views/ResourceListSwiftUIView.swift
@@ -0,0 +1,27 @@
+//
+//  ResourceListSwiftUIView.swift
+//  CleanAir
+//
+//  Created by Marko Engelman on 27/11/2020.
+//
+
+import SwiftUI
+
+struct ResourceListSwiftUIView<Resource, ResourceViewModel, ResourceView>: View where ResourceViewModel: Identifiable, ResourceView: ResourceSwiftUIView {
+  var onAppear: () -> Void
+  var builder: (ResourceViewModel) -> ResourceView
+  
+  @ObservedObject var viewModel: ResourceListViewModel<Resource, ResourceViewModel>
+  
+  var body: some View {
+    ScrollView(.vertical, showsIndicators: true, content: {
+      let coloumns = [GridItem(.flexible(maximum: .infinity))]
+      LazyVGrid(columns: coloumns, alignment: .leading) {
+        ForEach(viewModel.resource) { resource in
+          builder(resource)
+        }
+      }
+    })
+    .onAppear(perform: { onAppear() })
+  }
+}
