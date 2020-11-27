fa7a771b8e8101048f550564c8c14721a8a14a35
Marko Engelman <engleman.marko@gmail.com>
2020-11-27 14:58:13 +0100

    Add 'FavouriteCityListSwiftUIView'

---
+++ CleanAir/UI/Cities/Views/FavouriteList/FavouriteCityListSwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  FavouriteCityListSwiftUIView.swift
+//  CleanAir
+//
+//  Created by Marko Engelman on 27/11/2020.
+//
+
+import SwiftUI
+
+struct FavouriteCityListSwiftUIView: View {
+  var body: some View {
+    Text("")
+  }
+}
+
+struct FavouriteCityListSwiftUIView_Previews: PreviewProvider {
+  static var previews: some View {
+    FavouriteCityListSwiftUIView()
+  }
+}
