13f3fcf0767ec67183146573bbc7db6ced09d916
Marko Engelman <engleman.marko@gmail.com>
2020-11-27 15:01:07 +0100

    Add 'ResourceListViewModel' - simplified version of 'ResourceLoadingListViewModel'

---
+++ CleanAir/UI/Shared/Views/ResourceListViewModel.swift
@@ -0,0 +1,52 @@
+//
+//  ResourceListViewModel.swift
+//  CleanAir
+//
+//  Created by Marko Engelman on 27/11/2020.
+//
+
+import Foundation
+import CleanAirPresentation
+
+class ResourceListViewModel<Resource, ResourceViewModel>: ObservableObject {
+  typealias ViewModelMapper = (Resource) -> ResourceViewModel
+  let onAppear: () -> Void
+  let selection: (Resource) -> Void
+  let mapper: ViewModelMapper
+  
+  @Published var resource: [ResourceViewModel]
+  @Published var error: String?
+  
+  init(onAppear: @escaping () -> Void,
+       onSelect: @escaping (_ resource: Resource) -> Void,
+       resource: [ResourceViewModel]) where Resource == ResourceViewModel {
+    self.onAppear = onAppear
+    self.resource = resource
+    self.selection = onSelect
+    self.mapper = { $0 }
+  }
+  
+  init(onAppear: @escaping () -> Void,
+       onSelect: @escaping (_ resource: Resource) -> Void,
+       mapper: @escaping ViewModelMapper,
+       resource: [ResourceViewModel] = []) {
+    self.onAppear = onAppear
+    self.resource = resource
+    self.selection = onSelect
+    self.mapper = mapper
+  }
+}
+
+// MARK: - ResourceView
+extension ResourceListViewModel: ResourceView {
+  func show(resourceViewModel: [Resource]) {
+    self.resource = resourceViewModel.map { mapper($0) }
+  }
+}
+
+// MARK: - ResourceErrorView
+extension ResourceListViewModel: ResourceErrorView {
+  func show(errorViewModel: ResourceErrorViewModel) {
+    error = errorViewModel.error
+  }
+}
