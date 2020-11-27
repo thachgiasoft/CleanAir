2a777c2d26bf5a0a301f0d728e64e1e46b03fd1f
Marko Engelman <engleman.marko@gmail.com>
2020-11-27 15:01:58 +0100

    Add 'FavouriteCityListViewModel'.

---
+++ CleanAir/UI/Cities/ViewModels/FavouriteCityListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FavouriteCityListViewModel.swift
+//  CleanAir
+//
+//  Created by Marko Engelman on 27/11/2020.
+//
+
+import Foundation
+import CleanAirPresentation
+import CleanAirModules
+
+typealias FavouriteCityListViewModel = ResourceListViewModel<City, CityViewModel>
