bf3ef376abe589dc4070baca73820d79b033503d
Marko Engelman <engleman.marko@gmail.com>
2020-11-27 15:31:11 +0100

    Compose 'FavouriteCityListSwiftUIView'.

--- CleanAir/UI/Cities/Views/FavouriteList/FavouriteCityListSwiftUIView.swift
+++ CleanAir/UI/Cities/Views/FavouriteList/FavouriteCityListSwiftUIView.swift
@@ -9,10 +9,23 @@ import SwiftUI
 
 struct FavouriteCityListSwiftUIView: View {
   var onAppear: () -> Void
+  var onAdd: () -> Void
   @ObservedObject var viewModel: FavouriteCityListViewModel
   
   var body: some View {
-    Text("")
+    List(viewModel.resource, id: \.self.name) { city in
+      FavouriteCitySwiftUIView(viewModel: city)
+    }.onAppear(perform: {
+      onAppear()
+    })
+    .navigationBarItems(
+      trailing:
+        Button(action: {
+          onAdd()
+        }) {
+          Image(systemName: "plus")
+        }
+    )
   }
 }
 
@@ -20,6 +33,7 @@ struct FavouriteCityListSwiftUIView_Previews: PreviewProvider {
   static var previews: some View {
     FavouriteCityListSwiftUIView(
       onAppear: {  },
+      onAdd: { },
       viewModel: ResourceListViewModel(
         onAppear: {  }, onSelect: { _ in },
         mapper: CityViewModelMapper.map
