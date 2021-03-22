7b1577c989f94a80af147df910b93fcaeaaa27a5
Marko Engelman <engleman.marko@gmail.com>
2021-03-22 17:03:49 +0100

    Compose 'RootCoordinator'.

--- CleanAir/Root/AppFlow/RootCoordinator.swift
+++ CleanAir/Root/AppFlow/RootCoordinator.swift
@@ -7,6 +7,15 @@
 
 import Foundation
 
-final class RootCoordinator {
+final class RootCoordinator<RootView> where RootView: PresentingView {
+  typealias View = RootView.View
+  let initialView: RootView
   
+  init(initialView: RootView) {
+    self.initialView = initialView
+  }
+  
+  func start(with view: () -> View) {
+    initialView.show(view: view())
+  }
 }
