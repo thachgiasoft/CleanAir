130f9c1a7ac28bf6ee5bb2f3bad1c2e45cf001bd
Marko Engelman <engleman.marko@gmail.com>
2021-03-22 18:12:56 +0100

    Cleanup 'AppDelegate'.

--- CleanAir/Root/App_iOS/AppDelegate.swift
+++ CleanAir/Root/App_iOS/AppDelegate.swift
@@ -30,47 +30,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   }
 }
 
-// MARK: - Private
-private extension AppDelegate {
-  func openCountry(country: Country) {
-    rootController?.show(citiviesView(for: country), sender: self)
+// MARK: - Root
+extension UIViewController: PresentingView {
+  func show(view: UIViewController) {
+    show(view, sender: self)
   }
-  
-  func openCity(city: City) {
-    let loader = MeasurementsComponentsComposer.measurementsLoader(client: Self.makeHTTPClient(), cityId: city.id)
-    let controller = MeasurementsUIComposer.makeMeasurementsView(with: loader)
-    rootController?.show(controller, sender: self)
-  }
-  
-  func showCountries() {
-    rootController?.show(countriresView(), sender: self)
-  }
-  
-  func countriresView() -> UIViewController {
-    let loader = CountriesComponentsComposer.countriesLoader(client: Self.makeHTTPClient(), realm: Self.makeRealm)
-    let controller = CountriesUIComposer.makeView(with: loader, selection: { [weak self] in self?.openCountry(country: $0) })
-    return controller
-  }
-  
-  func favouriteCitiesView() -> UIViewController {
-    let service = CitiesComponentsComposer.countriesFavouriteService(realm: Self.makeRealm)
-    let storage = CitiesComponentsComposer.storage(realm: Self.makeRealm)
-    let controller = CitiesUIComposer.makeFavouritesView(
-      with: storage,
-      service: service,
-      onSelect: { [weak self] in self?.openCity(city: $0) },
-      onAdd: { [weak self] in self?.showCountries() })
-    return controller
-  }
-  
-  func citiviesView(for country: Country) -> UIViewController {
-    let storage = CitiesComponentsComposer.storage(realm: Self.makeRealm)
-    let loader = CitiesComponentsComposer.countriesLoader(client: Self.makeHTTPClient(), storage: storage, countryCode: country.code)
-    let service = CitiesComponentsComposer.countriesFavouriteService(realm: Self.makeRealm)
-    let controller = CitiesUIComposer.makeView(with: loader, service: service, onSelect: { [weak self] in self?.openCity(city: $0) })
-    return controller
-  }
-  
+}
+
+extension AppDelegate {
   static func makeHTTPClient() -> HTTPClient {
     let client = URLSessionHTTPClient(session: .shared)
     return HTTPClientLogger(client: client)
@@ -78,13 +45,6 @@ private extension AppDelegate {
   
   static func makeRealm() -> Realm {
     return try! Realm()
-  }
-}
-
-// MARK: - Root
-extension UIViewController: PresentingView {
-  func show(view: UIViewController) {
-    show(view, sender: self)
   }
 }
 
